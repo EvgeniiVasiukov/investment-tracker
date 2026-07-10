56bb46fc4c270040720c6e73c3db7a26d6718416
Evgenii <evgenii@Evgeniis-MacBook-Pro.local>
2026-07-10 12:30:36 +0200

    Credit & CreditPayment entities

---
+++ services/credit-service/credit-service/src/main/java/com/investmenttracker/creditservice/entity/CreditPayment.java
@@ -0,0 +1,46 @@
+package com.investmenttracker.creditservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "credit_payments")
+public class CreditPayment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "credit_id", nullable = false)
+    private Credit credit;
+
+    @Column(name = "payment_date", nullable = false)
+    private LocalDate paymentDate;
+
+    @Column(name = "amount", nullable = false, precision = 19, scale = 2)
+    private BigDecimal amount;
+
+    @Column(name = "payment_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    CreditPaymentType paymentType;
+
+    @Column(name = "payment_source", nullable = false)
+    @Enumerated(EnumType.STRING)
+    CreditPaymentSource paymentSource;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+}
