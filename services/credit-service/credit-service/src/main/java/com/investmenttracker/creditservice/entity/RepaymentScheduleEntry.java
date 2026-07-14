6dc150937a7dd264c28580203b8950b26a1eb280
Evgenii <evgenii@Evgeniis-MacBook-Pro.local>
2026-07-14 18:39:20 +0200

    RepaymentScheduleEntry with all annotations created

--- services/credit-service/credit-service/src/main/java/com/investmenttracker/creditservice/entity/RepaymentScheduleEntry.java
+++ services/credit-service/credit-service/src/main/java/com/investmenttracker/creditservice/entity/RepaymentScheduleEntry.java
@@ -1,11 +1,13 @@
 package com.investmenttracker.creditservice.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "repayment_schedule_entries")
@@ -14,5 +16,31 @@ import lombok.Setter;
 @Setter
 public class RepaymentScheduleEntry {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "credit_id", nullable = false)
+    private Credit credit;
+    @Enumerated(EnumType.STRING)
+    private RepaymenScheduleEntryStatus status;
+    @Column(name = "installment_number", nullable = false)
+    private Integer instalmentNumber;
+    @Column(name="payment_date", nullable = false)
+    private LocalDate paymentDate;
+    @Column(name = "total_payment_amount", nullable = false, precision = 19, scale = 2)
+    private BigDecimal totalPaymentAmount;
+
+    @Column(name = "principal_amount", nullable = false, precision = 19, scale = 2)
+    private BigDecimal principalAmount;
+
+    @Column(name = "interest_amount", nullable = false, precision = 19, scale = 2)
+    private BigDecimal interestAmount;
+
+    @Column(name = "remaining_principal_amount", nullable = false, precision = 19, scale = 2)
+    private BigDecimal remainingPrincipalAmount;
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
 
 }
