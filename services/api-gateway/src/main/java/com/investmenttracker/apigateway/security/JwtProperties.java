4c1dbbae933d3543eced23fa1ff9f2e5be9cf540
Evgenii <evgenii@evgeniis-mbp.speedport.ip>
2026-07-22 23:50:49 +0200

    created JwtProperties class

---
+++ services/api-gateway/src/main/java/com/investmenttracker/apigateway/security/JwtProperties.java
@@ -0,0 +1,11 @@
+package com.investmenttracker.apigateway.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+    private String secret;
+    private Integer expiration;
+}
