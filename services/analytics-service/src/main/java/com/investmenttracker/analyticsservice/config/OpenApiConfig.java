b246105c8af95eb23914d08e1daedbbca773a28b
Evgenii <evgenii@Evgeniis-MacBook-Pro.local>
2026-07-24 15:42:30 +0200

    swagger congig

--- services/analytics-service/src/main/java/com/investmenttracker/analyticsservice/config/OpenApiConfig.java
+++ services/analytics-service/src/main/java/com/investmenttracker/analyticsservice/config/OpenApiConfig.java
@@ -1,4 +1,17 @@
 package com.investmenttracker.analyticsservice.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
+    @Bean
+    public OpenAPI analyticsServerOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Investment Tracker - Analytics-service")
+                        .description("Provides aggregated analytics and financial insights based on user portfolio")
+                        .version("1.0.0"));
+    }
 }
