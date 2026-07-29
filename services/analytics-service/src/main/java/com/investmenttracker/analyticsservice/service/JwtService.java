1d5a16f309a72628b08f4146176f5f1b8514bc9b
Evgenii <evgenii@Evgeniis-MacBook-Pro.local>
2026-07-29 15:24:47 +0200

    jwtService implemented

--- services/analytics-service/src/main/java/com/investmenttracker/analyticsservice/service/JwtService.java
+++ services/analytics-service/src/main/java/com/investmenttracker/analyticsservice/service/JwtService.java
@@ -1,6 +1,9 @@
 package com.investmenttracker.analyticsservice.service;
 
 import com.investmenttracker.analyticsservice.config.properties.JwtProperties;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +20,21 @@ public class JwtService {
     private SecretKey getSecretKey() {
         byte [] keyBytes = properties.getSecret().getBytes(StandardCharsets.UTF_8);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+    public String extractUserId(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+    public String extractStatus(String token) {
+        return extractAllClaims(token).get("status", String.class);
+    }
+    public String extractRole(String token) {
+        return extractAllClaims(token).get("role", String.class);
     }
 }
