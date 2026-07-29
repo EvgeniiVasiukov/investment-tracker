b598c63899849422ea84ae0a97f9f33b4e0ccd32
Evgenii <evgenii@Evgeniis-MacBook-Pro.local>
2026-07-29 16:50:36 +0200

    jwtAuthenticationFilter added is security config

--- services/analytics-service/src/main/java/com/investmenttracker/analyticsservice/config/SecurityConfig.java
+++ services/analytics-service/src/main/java/com/investmenttracker/analyticsservice/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.investmenttracker.analyticsservice.config;
 import com.investmenttracker.analyticsservice.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -26,6 +28,8 @@ public class SecurityConfig {
                                 "/test"
                         ).permitAll()
                         .anyRequest().authenticated())
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
