258f03848f2ee05f5f20d73e0e11d6d250420434
Evgenii <evgenii@Evgeniis-MacBook-Pro.local>
2026-08-04 14:57:22 +0200

    Credit client

---
+++ services/analytics-service/src/main/java/com/investmenttracker/analyticsservice/client/CreditClient.java
@@ -0,0 +1,20 @@
+package com.investmenttracker.analyticsservice.client;
+
+import com.investmenttracker.analyticsservice.dto.CreditResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.client.RestClient;
+
+public class CreditClient {
+    private final RestClient restClient;
+    public CreditClient(@Qualifier("creditRestClient") RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    public CreditResponse getCredit(String authorizationHeader) {
+        return restClient.get()
+                .uri("/credits/me")
+                .header("Authorization", authorizationHeader)
+                .retrieve()
+                .body(CreditResponse.class);
+    }
+}
