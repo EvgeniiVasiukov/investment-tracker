2f313a9916a31979cc2b1bc76f5f7e7a63d752c8
Evgenii <evgenii@Evgeniis-MacBook-Pro.local>
2026-08-04 14:44:41 +0200

    Bean for credit client cvreated

--- services/analytics-service/src/main/java/com/investmenttracker/analyticsservice/config/RestClientConfig.java
+++ services/analytics-service/src/main/java/com/investmenttracker/analyticsservice/config/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.investmenttracker.analyticsservice.config;
 
+import com.investmenttracker.analyticsservice.config.properties.CreditServiceProperties;
 import com.investmenttracker.analyticsservice.config.properties.MarketDataServiceProperties;
 import com.investmenttracker.analyticsservice.config.properties.PortfolioServiceProperties;
 import org.springframework.context.annotation.Bean;
@@ -10,11 +11,14 @@ import org.springframework.web.client.RestClient;
 public class RestClientConfig {
     private final PortfolioServiceProperties portfolioServiceProperties;
     private final MarketDataServiceProperties marketDataServiceProperties;
+    private final CreditServiceProperties creditServiceProperties;
 
     public RestClientConfig(PortfolioServiceProperties portfolioServiceProperties,
-                            MarketDataServiceProperties marketDataServiceProperties) {
+                            MarketDataServiceProperties marketDataServiceProperties,
+                            CreditServiceProperties creditServiceProperties) {
         this.portfolioServiceProperties = portfolioServiceProperties;
         this.marketDataServiceProperties = marketDataServiceProperties;
+        this.creditServiceProperties = creditServiceProperties;
     }
     @Bean
     public RestClient portfolioRestClient() {
@@ -26,6 +30,12 @@ public class RestClientConfig {
     public RestClient marketRestClient() {
         return RestClient.builder()
                 .baseUrl(marketDataServiceProperties.getBaseUrl())
+                .build();
+    }
+    @Bean
+    public RestClient creditRestClient() {
+        return RestClient.builder()
+                .baseUrl(creditServiceProperties.getBaseUrl())
                 .build();
     }
 }
