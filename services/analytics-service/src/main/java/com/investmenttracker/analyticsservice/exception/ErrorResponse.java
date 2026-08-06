abf879010046326c08e97053ad81149973776879
Evgenii <evgenii@Evgeniis-MacBook-Pro.local>
2026-08-06 12:56:08 +0200

    carcassus for error handling

---
+++ services/analytics-service/src/main/java/com/investmenttracker/analyticsservice/exception/ErrorResponse.java
@@ -0,0 +1,4 @@
+package com.investmenttracker.analyticsservice.exception;
+
+public record ErrorResponse() {
+}
---
+++ services/analytics-service/src/main/java/com/investmenttracker/analyticsservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.investmenttracker.analyticsservice.exception;
+
+public class GlobalExceptionHandler {
+}
