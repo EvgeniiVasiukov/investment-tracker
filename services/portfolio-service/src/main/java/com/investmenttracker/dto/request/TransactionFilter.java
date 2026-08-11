18080ab6b740d06a1e5547eddea790a669a95469
Evgenii <evgenii@Evgeniis-MacBook-Pro.local>
2026-08-11 17:23:34 +0200

    Transaction filter added

---
+++ services/portfolio-service/src/main/java/com/investmenttracker/dto/request/TransactionFilter.java
@@ -0,0 +1,14 @@
+package com.investmenttracker.dto.request;
+
+import com.investmenttracker.entity.TransactionType;
+
+import java.time.LocalDateTime;
+
+public record TransactionFilter(
+        String ticker,
+        TransactionType transactionType,
+        LocalDateTime dateFrom,
+        LocalDateTime dateTo,
+        Long userId
+) {
+}
