2be3048e09f074e305126c3043fe8de6da500421
Evgenii <evgenii@Evgeniis-MacBook-Pro.local>
2026-08-12 14:56:52 +0200

    finalization

--- services/analytics-service/src/main/java/com/investmenttracker/analyticsservice/client/PortfolioClient.java
+++ services/analytics-service/src/main/java/com/investmenttracker/analyticsservice/client/PortfolioClient.java
@@ -1,6 +1,7 @@
 package com.investmenttracker.analyticsservice.client;
 
 import com.investmenttracker.analyticsservice.dto.PortfolioPositionsPageDto;
+import com.investmenttracker.analyticsservice.dto.TransactionPageDto;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
