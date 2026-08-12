391a0c14ab1328e2526e4edc4ffb361b1e5a5a2b
Evgenii <evgenii@Evgeniis-MacBook-Pro.local>
2026-08-12 14:31:35 +0200

    transaction dto added

--- services/analytics-service/src/main/java/com/investmenttracker/analyticsservice/dto/TransactionDto.java
+++ services/analytics-service/src/main/java/com/investmenttracker/analyticsservice/dto/TransactionDto.java
@@ -1,8 +1,20 @@
 package com.investmenttracker.analyticsservice.dto;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Currency;
+
 public record TransactionDto(
     Long id,
-    Tr
+    TransactionType transactionType,
+    String ticker,
+    BigDecimal quantity,
+    BigDecimal price,
+    Currency currency,
+    BigDecimal fees,
+    BigDecimal tax,
+    BigDecimal realizedProfitLoss,
+    LocalDateTime transactionDate
 
 ) {
 }
